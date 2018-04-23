88e19b511df3576a39e39562925c1cadd82aa5ee
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2018-04-23 10:11:22 +0200

    bugfix fixed module name to be compatible to TYPO3 6.2
    #2

--- mod/ext_tables.php
+++ mod/ext_tables.php
@@ -7,7 +7,7 @@ if (TYPO3_MODE == 'BE') {
     tx_rnbase_util_Extensions::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:'.$_EXTKEY.'/mod/pageTSconfig.txt">');
 
     tx_rnbase_util_Extensions::registerModule(
-        'Txt3rest',
+        't3rest',
         'user',
         'M1',
         'bottom',
@@ -21,7 +21,7 @@ if (TYPO3_MODE == 'BE') {
     );
 
     tx_rnbase_util_Extensions::insertModuleFunction(
-        'user_Txt3restM1',
+        'user_T3restM1',
         'tx_t3rest_mod_Logs',
         tx_rnbase_util_Extensions::extPath('t3rest', 'mod/class.tx_t3rest_mod_Logs.php'),
         'LLL:EXT:t3rest/mod/locallang.xml:label_t3rest_mod_logs'
