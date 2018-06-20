989da46d0e5410ea9e63d028cbb402b60b801e8c
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.de>
2018-06-20 09:28:36 +0200

    bugfix access to sitename

--- Classes/Routines/Auth/FeUser.php
+++ Classes/Routines/Auth/FeUser.php
@@ -121,7 +121,7 @@ class Tx_T3rest_Routines_Auth_FeUser implements
         }
 
         // no access, don't process the route!
-        header('WWW-Authenticate: Basic realm="' . $GLOBALS['TSFE']->TYPO3_CONF_VARS['SYS']['sitename'] . '"');
+        header('WWW-Authenticate: Basic realm="' . $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] . '"');
         \TYPO3\CMS\Core\Utility\HttpUtility::setResponseCode(
             \TYPO3\CMS\Core\Utility\HttpUtility::HTTP_STATUS_401
         );
