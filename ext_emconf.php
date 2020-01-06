d5bab78d0ed2a6d9018a4e3f009e7047805bcea1
Mario Seidel <mario.seidel@dmk-ebusiness.de>
2020-01-06 11:49:30 +0100

    drop support for TYPO3 below Version 7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'lockType' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.2.0-8.7.99',
+            'typo3' => '7.6.0-9.5.99',
             'rn_base' => '1.7.1-',
         ),
         'conflicts' => array(
