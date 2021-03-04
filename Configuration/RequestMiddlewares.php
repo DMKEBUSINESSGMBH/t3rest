0462e8c56d7f60e7d56b62e9a7766970c1d6dc92
Michael Wagner <michael.wagner@dmk-ebusiness.com>
2021-03-04 09:03:43 +0100

    Fix middleware order in typo3 10

--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -2,19 +2,21 @@
 
 return [
     'frontend' => [
-        't3rest-authmapper' => [
+        'dmk/t3rest/authmapper' => [
             'target' => \DMK\T3rest\Middleware\AuthResolver::class,
             'before' => [
                 'typo3/cms-frontend/authentication',
             ],
         ],
-        't3rest-api' => [
+        'dmk/t3rest/api' => [
             'target' => \DMK\T3rest\Middleware\RestApiMiddleware::class,
             'after' => [
                 'typo3/cms-frontend/authentication',
             ],
             'before' => [
-                'typo3/cms-frontend/site',
+                Sys25\RnBase\Utility\TYPO3::isTYPO104OrHigher() ?
+                    'typo3/cms-frontend/base-redirect-resolver' :
+                    'typo3/cms-frontend/site',
             ],
         ],
     ],
