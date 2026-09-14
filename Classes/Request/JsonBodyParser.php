c27e1812febf9114f7d234ed9fde61aecf98b336
Hannes Bochmann <hannes.bochmann@dmk-ebusiness.com>
2026-09-14 09:08:41 +0200

    Bugfix add support for guzzlehttp 8.x

--- Classes/Request/JsonBodyParser.php
+++ Classes/Request/JsonBodyParser.php
@@ -49,6 +49,6 @@ class JsonBodyParser implements BodyParserInterface
             return [];
         }
 
-        return \GuzzleHttp\json_decode($content, true) ?: [];
+        return \json_decode($content, true, 512, \JSON_THROW_ON_ERROR) ?: [];
     }
 }
